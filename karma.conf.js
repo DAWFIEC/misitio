55ab9e9ae094e9985fe1e8b3f2c0939196856731
unknown <aavendan@gmail.com>
2022-01-05 08:05:45 -0500

    latest

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sitio'),
+      dir: require('path').join(__dirname, './coverage/sitio-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
